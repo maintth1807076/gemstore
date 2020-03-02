9b22c851854fe2aea00b50e11ac621cd5ac95263
maintth1807076 <maintth1807076@fpt.edu.vn>
2020-03-02 23:56:34 +0700

    fix problem build fail

--- GemStore/Models/ManageModel.cs
+++ GemStore/Models/ManageModel.cs
@@ -5,7 +5,12 @@ using System.Web;
 
 namespace GemStore.Models
 {
-    public class ManageModel
+    public class Item
     {
+        public string Id { get; set; }
+    }
+    public class Product
+    {
+        public string Id { get; set; }
     }
 }
