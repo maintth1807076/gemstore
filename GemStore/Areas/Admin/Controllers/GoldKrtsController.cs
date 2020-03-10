1deae6330eb40e2f334c214da445874d1dbcc860
OliverQueen <tung0963242091@gmail.com>
2020-03-10 21:37:25 +0700

    create table admin

---
+++ GemStore/Areas/Admin/Controllers/GoldKrtsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GemStore.Models;
+
+namespace GemStore.Areas.Admin.Controllers
+{
+    public class GoldKrtsController : Controller
+    {
+        private GemStoreContext db = new GemStoreContext();
+
+        // GET: Admin/GoldKrts
+        public ActionResult Index()
+        {
+            return View(db.GoldKrts.ToList());
+        }
+
+        // GET: Admin/GoldKrts/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GoldKrt goldKrt = db.GoldKrts.Find(id);
+            if (goldKrt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(goldKrt);
+        }
+
+        // GET: Admin/GoldKrts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/GoldKrts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GoldTypeId,GoldCrt")] GoldKrt goldKrt)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GoldKrts.Add(goldKrt);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(goldKrt);
+        }
+
+        // GET: Admin/GoldKrts/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GoldKrt goldKrt = db.GoldKrts.Find(id);
+            if (goldKrt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(goldKrt);
+        }
+
+        // POST: Admin/GoldKrts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "GoldTypeId,GoldCrt")] GoldKrt goldKrt)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(goldKrt).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(goldKrt);
+        }
+
+        // GET: Admin/GoldKrts/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GoldKrt goldKrt = db.GoldKrts.Find(id);
+            if (goldKrt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(goldKrt);
+        }
+
+        // POST: Admin/GoldKrts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            GoldKrt goldKrt = db.GoldKrts.Find(id);
+            db.GoldKrts.Remove(goldKrt);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GemStore/Areas/Admin/Controllers/StoneQltyMstsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GemStore.Models;
+
+namespace GemStore.Areas.Admin.Controllers
+{
+    public class StoneQltyMstsController : Controller
+    {
+        private GemStoreContext db = new GemStoreContext();
+
+        // GET: Admin/StoneQltyMsts
+        public ActionResult Index()
+        {
+            return View(db.StoneQltyMsts.ToList());
+        }
+
+        // GET: Admin/StoneQltyMsts/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StoneQltyMst stoneQltyMst = db.StoneQltyMsts.Find(id);
+            if (stoneQltyMst == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stoneQltyMst);
+        }
+
+        // GET: Admin/StoneQltyMsts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/StoneQltyMsts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StoneQltyID,StoneQlty,Description")] StoneQltyMst stoneQltyMst)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StoneQltyMsts.Add(stoneQltyMst);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(stoneQltyMst);
+        }
+
+        // GET: Admin/StoneQltyMsts/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StoneQltyMst stoneQltyMst = db.StoneQltyMsts.Find(id);
+            if (stoneQltyMst == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stoneQltyMst);
+        }
+
+        // POST: Admin/StoneQltyMsts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StoneQltyID,StoneQlty,Description")] StoneQltyMst stoneQltyMst)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(stoneQltyMst).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(stoneQltyMst);
+        }
+
+        // GET: Admin/StoneQltyMsts/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StoneQltyMst stoneQltyMst = db.StoneQltyMsts.Find(id);
+            if (stoneQltyMst == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stoneQltyMst);
+        }
+
+        // POST: Admin/StoneQltyMsts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            StoneQltyMst stoneQltyMst = db.StoneQltyMsts.Find(id);
+            db.StoneQltyMsts.Remove(stoneQltyMst);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
