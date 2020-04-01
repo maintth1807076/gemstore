3354fe95ea8fd25b781d52df79d637ab7e53b9c5
maintth1807076 <maintth1807076@fpt.edu.vn>
2020-04-01 14:19:48 +0700

    dashbroad

---
+++ GemStore/Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using GemStore.Models;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace GemStore.Areas.Admin.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private GemStoreContext db = new GemStoreContext();
+        public async Task<ActionResult> ChangeStatus(string id, int status)
+        {
+            var order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound, "Order's' not found");
+            }
+
+            if (status == (int)Order.OrderStatus.Confirmed)
+            {
+                var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                await userManager.SendEmailAsync(order.MemberId, "Confirm your order", "Please confirm your order");
+            }
+            order.Status = status;
+            db.SaveChanges();
+            return RedirectToAction("");
+        }
+        // GET: Admin/Orders
+        public ActionResult Index(DateTime? createdAt)
+        {
+            //var listOrder = db.Orders.Where(o => o.Status != (int)Order.OrderStatus.Deleted);
+            var listOrder = db.Orders.Where(o => o.CreatedAt == createdAt);
+            Debug.WriteLine(createdAt);
+            if (createdAt != null)
+            {
+                listOrder = listOrder.Where(o => o.CreatedAt == createdAt);
+            }
+            return View(listOrder.ToList());
+        }
+
+        // GET: Admin/Orders/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Orders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OrderId,MemberId,PaymentTypeId,ShipName,ShipAddress,ShipPhone,TotalPrice,CreatedAt,UpdatedAt,DeletedAt,Status")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Admin/Orders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OrderId,MemberId,PaymentTypeId,ShipName,ShipAddress,ShipPhone,TotalPrice,CreatedAt,UpdatedAt,DeletedAt,Status")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Admin/Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
